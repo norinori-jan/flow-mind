379eb89a76932859fe246e402a1279c549b4963c
norinori-jan <161024651+norinori-jan@users.noreply.github.com>
2026-07-26 13:22:29 +0900

    Add CORS-enabled receive API

---
+++ functions/api/receive.js
@@ -0,0 +1,25 @@
+export async function onRequestPost(context) {
+  const data = await context.request.json();
+
+  return new Response(JSON.stringify({
+    ok: true,
+    received: data
+  }), {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    }
+  });
+}
+
+export async function onRequestOptions() {
+  return new Response(null, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    }
+  });
+}
