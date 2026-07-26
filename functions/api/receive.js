be522d39a0662ba9fe907cfa513200f7771d3a71
norinori-jan <161024651+norinori-jan@users.noreply.github.com>
2026-07-26 13:41:43 +0900

    Fix CORS for receive API

--- functions/api/receive.js
+++ functions/api/receive.js
@@ -1,20 +1,4 @@
-export async function onRequestPost(context) {
-  const data = await context.request.json();
-
-  return new Response(JSON.stringify({
-    ok: true,
-    received: data
-  }), {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type"
-    }
-  });
-}
-
-export async function onRequestOptions() {
+export async function onRequestOptions(context) {
   return new Response(null, {
     headers: {
       "Access-Control-Allow-Origin": "*",
@@ -22,4 +6,34 @@ export async function onRequestOptions() {
       "Access-Control-Allow-Headers": "Content-Type"
     }
   });
+}
+
+export async function onRequestPost(context) {
+  try {
+    const data = await context.request.json();
+
+    return new Response(JSON.stringify({
+      ok: true,
+      received: data
+    }), {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type"
+      }
+    });
+
+  } catch (err) {
+    return new Response(JSON.stringify({
+      ok: false,
+      error: err.message
+    }), {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      status: 500
+    });
+  }
 }
